f13463c507b35ecd26ca46d8fbd64d19673abbc8
Priyank gupta <priyaaank@gmail.com>
2016-04-27 16:56:44 +0530

    Added progress state to the ui build as well

--- jobs/ui_build_status.rb
+++ jobs/ui_build_status.rb
@@ -1,6 +1,7 @@
 SCHEDULER.every '5s' do
   require 'open-uri'
   require 'json'
+  image_hash = {"SUCCESS" => "/success.png", "FAILURE" => "/failure.png", "PROGRESS" => "/hourglass.png"}
   jenkins_url = ENV['JENKINS_HOST_PORT']
 
   basic_auth = [ENV['JENKINS_USERNAME'], ENV['JENKINS_TOKEN']]
@@ -8,8 +9,10 @@ SCHEDULER.every '5s' do
   functional_test_status_response = open("http://#{jenkins_url}/job/Functional-test-amazon-api/lastBuild/api/json", http_basic_authentication: basic_auth).read()
   json_ui_build_response = JSON.parse(ui_build_status_response)
   json_functional_build_response = JSON.parse(functional_test_status_response)
-  ui_latest_status_success=json_ui_build_response['result']=="SUCCESS"
-  functional_latest_status_success=json_functional_build_response['result']=="SUCCESS"
-  send_event('uibuild', { image: "#{ui_latest_status_success ? '/success.png' : '/failure.png'}", moreinfo: "code coverage not available",status: "#{ui_latest_status_success ? 'success' : 'failure'}"})
-  send_event('functionaltestbuild', { image: "#{functional_latest_status_success ? '/success.png' : '/failure.png'}", moreinfo: "",status: "#{functional_latest_status_success ? 'success' : 'failure'}"})
+  ui_latest_status_state=json_ui_build_response['result']||"PROGRESS"
+  functional_latest_status_state=json_functional_build_response['result']||"PROGRESS"
+  ui_image_name = image_hash[ui_latest_status_state]
+  funtional_image_name = image_hash[functional_latest_status_state]
+  send_event('uibuild', { image: ui_image_name, moreinfo: "code coverage not available",status: "#{ui_latest_status_state.downcase}"})
+  send_event('functionaltestbuild', { image: funtional_image_name, moreinfo: "",status: "#{functional_latest_status_state.downcase}"})
 end
